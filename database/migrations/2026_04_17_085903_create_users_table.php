e29ec7cab142e1ca9ef25d628b142c0e9bac783c
Jonathan Peguit Jr. <jonathanjrhayo@gmail.com>
2026-04-17 17:03:34 +0800

    first commit

---
+++ database/migrations/2026_04_17_085903_create_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('user_email', 255)->unique();
+            $table->string('user_password_hash',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
